8a05665db953357317b75a66f7610c6c4be1c7d5
m.yuldashev <yuldashevmuhiddin65@gmail.com>
2023-05-22 14:06:56 +0500

    done!

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        System.out.println(Math.max(first, second));
     }
 }
